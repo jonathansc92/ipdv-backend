b5cc7df1751bd90d0ce977640d3824997d9658e1
Jonathan Cruz <jonathansc92@gmail.com>
2023-10-14 22:08:00 -0300

    refactor: removido comentários

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,12 +10,6 @@ service('auth')->routes($routes);
 
 
 $routes->group('api', ['namespace' => 'App\Controllers\Api'], function ($routes) {
-    // $routes->group('cost-centers', function ($routes) {
-    //     $routes->get('/', 'CostCenterController::get');
-    //     $routes->get('/(:num)', 'CostCenterController::show/$1');
-    //     $routes->post('/', 'CostCenterController::store');
-    // });
-
     $routes->resource('cost-centers', ['controller' => 'CostCenterController']);
 
     $routes->group('users', function ($routes) {
